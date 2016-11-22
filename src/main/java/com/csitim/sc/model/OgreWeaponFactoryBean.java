f1f8c0ab5731f71480b1ef4c483ce769e8ed955b
csitim <csitim@gmail.com>
2016-11-22 15:09:07 +0100

    Implement a FactoryBean.

---
+++ src/main/java/com/csitim/sc/model/OgreWeaponFactoryBean.java
@@ -0,0 +1,27 @@
+package com.csitim.sc.model;
+
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * Created by Timea_Csiszar on 11/22/2016.
+ */
+public class OgreWeaponFactoryBean implements FactoryBean<Weapon> {
+
+    private static int counter = 0;
+
+    @Override
+    public Weapon getObject() throws Exception {
+        counter++;
+        return new Weapon("Weapon from factory #"+counter, 50);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Weapon.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
